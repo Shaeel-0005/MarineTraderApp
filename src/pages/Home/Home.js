f7d6ea1963ca05311fcfb8c6c806ecb43cd2ff8e
Shaeel-0005 <135009664+Shaeel-0005@users.noreply.github.com>
2023-12-27 11:25:09 +0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
 
-export default function Home() {
+import React from 'react';
+import Header from '../../components/header/header';
+import Banner from '../../components/banner/banner';
+import Spotlight from '../../components/spotlight/spotlight';
+import Services from '../../components/services_home/services';
+import Footer from '../../components/footer/footer';
+const HomePage = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homepage">
+      
+      <Header />
+      
+      <Banner/>
+
+      <Spotlight/>
+
+      <Services/>
+      
+      <Footer/>
+
+    </div>
+  );
+};
+
+export default HomePage;
